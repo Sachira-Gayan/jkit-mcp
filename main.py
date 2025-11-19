377e0638ce8c0f5ca280343924911330b252eff6
Sachira Wijerathna <sachiraw@jkintranet.com>
2025-11-19 16:23:05 +0530

    Change text to json reponse

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastmcp import FastMCP
 import httpx
+import requests
 from config import header
-app = FastMCP("http-client-server")
+app = FastMCP("http-client-server",stateless_http=True)
 
 
 
@@ -10,41 +11,41 @@ app = FastMCP("http-client-server")
 # HTTP GET Tool
 # ------------------------------
 @app.tool()
-async def http_get(url: str) -> str:
+async def http_get(url: str) -> dict:
     """
     Perform an HTTP GET request and return the response text.
     """
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         # response.raise_for_status()
-        return response.text
+        return response.json()
 
 
 # ------------------------------
 # HTTP POST Tool
 # ------------------------------
 @app.tool()
-async def http_post(url: str, data: dict) -> str:
+async def http_post(url: str, data: dict) -> dict:
     """
     Perform an HTTP POST request with JSON body.
     """
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=data,headers=header)
         response.raise_for_status()
-        return response.text
+        return response.json()
 
 
 # ------------------------------
 # Call any API (headers + query)
 # ------------------------------
-@app.tool()
+@app.tool() 
 async def http_request(
     url: str,
     method: str = "GET",
     headers: dict = None,
     params: dict = None,
     json_body: dict = None,
-) -> str:
+) -> dict:
     """
     A flexible tool for GET/POST/PATCH/DELETE etc.
     """
@@ -57,7 +58,7 @@ async def http_request(
             json=json_body
         )
         response.raise_for_status()
-        return response.text
+        return response
 
 
 if __name__ == "__main__":
